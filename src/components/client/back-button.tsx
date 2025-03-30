f49452f56e97f30b9dba912d3fe584c1eeeac0db
Thiago Andrade Silva <thiago.adsix@gmail.com>
2025-03-30 19:51:26 -0300

    chore: remove unused BackButton component

--- src/components/client/back-button.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from "react";
-import { Button } from "@/components/ui/button";
-
-export const BackButton = () => {
-  return <Button onClick={() => history.back()}>Back to Blog list</Button>;
-};
